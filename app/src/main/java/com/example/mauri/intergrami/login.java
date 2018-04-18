28dd12d59f89de6e79fa18e9fd5a7664c33162d7
Mauricio Flores Hernández <mau1361317@gmail.com>
2018-04-18 00:07:01 -0500

    Este es un comentario, aquí pondrás con detalles los cambios que has hecho

--- app/src/main/java/com/example/mauri/intergrami/login.java
+++ app/src/main/java/com/example/mauri/intergrami/login.java
@@ -9,6 +9,9 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //comentario prueba xdxd
+        //El mencho, me pela la vrga v:
+        int xdxd=2000;
+
+
     }
 }
