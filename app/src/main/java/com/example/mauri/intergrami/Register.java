267998975a96e2da8bd24433928ece508f36751a
Mauricio Flores Hernández <mau1361317@gmail.com>
2018-04-18 20:27:56 -0500

    Se crearon los layouts Login y registro

--- app/src/main/java/com/example/mauri/intergrami/Register.java
+++ app/src/main/java/com/example/mauri/intergrami/Register.java
@@ -2,14 +2,12 @@ package com.example.mauri.intergrami;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_register);
     }
 }
